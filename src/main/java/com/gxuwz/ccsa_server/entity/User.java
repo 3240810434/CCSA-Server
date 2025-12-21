cf233ed93f3ee675bc89c86a7c19f84059f26a99
3240810434 <3240810434@qq.com>
2025-12-21 19:29:34 +0800

    1

--- src/main/java/com/gxuwz/ccsa_server/entity/User.java
+++ src/main/java/com/gxuwz/ccsa_server/entity/User.java
@@ -1,13 +1,16 @@
 package com.gxuwz.ccsa_server.entity;
 
-import jakarta.persistence.*; // 如果爆红，试换成 javax.persistence
+// 【关键修改】将 jakarta 改为 javax
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+
     private String name;
     private String gender;
     private String phone;
@@ -15,27 +18,70 @@ public class User {
     private String community;
     private String building;
     private String room;
-    private String avatar;
 
     public User() {}
 
-    // Getter Setter
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getCommunity() { return community; }
-    public void setCommunity(String community) { this.community = community; }
-    public String getBuilding() { return building; }
-    public void setBuilding(String building) { this.building = building; }
-    public String getRoom() { return room; }
-    public void setRoom(String room) { this.room = room; }
-    public String getAvatar() { return avatar; }
-    public void setAvatar(String avatar) { this.avatar = avatar; }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
--- src/main/java/com/gxuwz/ccsa_server/mapper/AdminMapper.java
+++
@@ -1,7 +0,0 @@
-package com.gxuwz.ccsa_server.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gxuwz.ccsa_server.entity.Admin;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AdminMapper extends BaseMapper<Admin> {}
